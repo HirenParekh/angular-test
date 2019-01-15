a23d3ef8b140b13699f22d9db3c9b61583b34aec
hiren.parekh <parekhhiren@outlook.com>
2019-01-15 23:57:51 +0530

    comments added and edit title functionality added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,7 @@
+/**
+ * this is the main component of application it will handle its child components and pass data and events between them.
+ */
+
 import { Component, OnInit } from '@angular/core';
 import { DataObj } from './models/data.model';
 import { DashboardService } from './services/dashboard.service';
@@ -9,8 +13,10 @@ import { DashboardService } from './services/dashboard.service';
 })
 export class DashboardComponent implements OnInit {
 
+  //stores all the data objects in memory and share it with its child components
   dataObjects:DataObj[];
 
+  //keep the reference of currently selected data object.
   selectedDataObj:DataObj;
 
   constructor(private dashboardService:DashboardService) { }
@@ -42,6 +48,11 @@ export class DashboardComponent implements OnInit {
   }
 
   onAddNewDataEventHandler(data){
+    /**
+     * store the new data item object on the local array of dataObjects
+     * it the application uses actual api we can call the service here and on a successful response
+     * we can update the local array of data Objects.
+     */
     this.dataObjects.push(data);
   }
 
